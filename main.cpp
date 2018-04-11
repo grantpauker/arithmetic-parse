372095b0e0849c7f1f0ac80a657377aa811b74be
ruwix <grantpauker@gmail.com>
2018-04-11 16:28:15 -0700

    implement tokenizer changes in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,18 +6,8 @@
 #include <iostream>
 int main()
 {
-    Constants constants();
-    std::string source = "123+34";
-    std::string::iterator it = source.begin();
-    std::vector<Token *> tokens;
-    tokens.push_back(Token::GetToken(&it));
-    std::cout << *it << std::endl;
-    for (std::vector<Token *>::iterator it2 = tokens.begin(); it2 < tokens.end(); it2++)
-    {
-        (*it2)->Print();
-    }
-    // std::string source = ;
-    // Tokenizer t("d");
-    // t.Tokenize();
+    Tokenizer t("123+34sdasdasd2312314fmds-2");
+    t.Tokenize();
+    t.Print();
     return 0;
 }
