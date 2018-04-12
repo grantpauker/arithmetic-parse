0b929bcebc9ce88580191ac80f2caaf8ff2ba5f6
ruwix <grantpauker@gmail.com>
2018-04-12 08:14:40 -0700

    add temporary operator handling in gettoken

--- token.cpp
+++ token.cpp
@@ -1,10 +1,13 @@
+#include <iostream>
 #include "token.hpp"
 #include "number_token.hpp"
-#include <iostream>
+#include "symbols.hpp"
 Token *Token::GetToken(std::string::iterator *it)
 {
     std::string::iterator tmp_it = *it;
     std::string tok_str;
+    Symbols sym;
+    sym = Symbols();
     if (isdigit(*tmp_it))
     {
         while (isdigit(*tmp_it))
@@ -15,9 +18,12 @@ Token *Token::GetToken(std::string::iterator *it)
         *it = tmp_it;
         return new NumberToken(std::stoi(tok_str));
     }
-    while (!isdigit(**it))
-    {
-        (*it)++;
+    else if (sym.IsOperator(*tmp_it)){
+        std::cout << "IsOperator: " << *tmp_it << std::endl;
     }
+        while (!isdigit(**it))
+        {
+            (*it)++;
+        }
     return NULL;
 }
