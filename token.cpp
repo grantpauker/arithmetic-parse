2f67aecb1e874167a8c97250569652e4fac1175a
ruwix <grantpauker@gmail.com>
2018-04-12 20:22:00 -0700

    add operator handling to get token

--- token.cpp
+++ token.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include "token.hpp"
 #include "number_token.hpp"
+#include "operator_token.hpp"
 #include "symbols.hpp"
 Token *Token::GetToken(std::string::iterator *it)
 {
     std::string::iterator tmp_it = *it;
     std::string tok_str;
-    Symbols sym;
-    sym = Symbols();
     if (isdigit(*tmp_it))
     {
         while (isdigit(*tmp_it))
@@ -18,12 +17,15 @@ Token *Token::GetToken(std::string::iterator *it)
         *it = tmp_it;
         return new NumberToken(std::stoi(tok_str));
     }
-    else if (sym.IsOperator(*tmp_it)){
-        std::cout << "IsOperator: " << *tmp_it << std::endl;
+    else if (IsOperator(*tmp_it))
+    {
+        tmp_it++;
+        tok_str = std::string(*it, tmp_it);
+        *it = tmp_it;
+        return new OperatorToken(operator_kinds.at(tok_str.c_str()[0]));
     }
-        while (!isdigit(**it))
-        {
-            (*it)++;
-        }
-    return NULL;
+    while (!isdigit(**it))
+    {
+        (*it)++;
+    }
 }
