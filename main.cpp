cb9fbfad53ef2120e124825345281965d0377899
ruwix <grantpauker@gmail.com>
2018-04-13 17:21:51 -0700

    Update main to fit  new classes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
-#include "token.hpp"
-#include "tokenizer.hpp"
-#include "symbols.hpp"
 #include <vector>
 #include <string>
 #include <iostream>
+#include <signal.h>
+#include "token.hpp"
+#include "tokenizer.hpp"
+#include "symbols.hpp"
+#include "token_handler.hpp"
+#include "shunting_yard.hpp"
+
 int main()
 {
     Tokenizer t("12+34/56%7&89");
-    t.Tokenize();
-    t.Print();
+    std::vector<Token *> tokens = t.Tokenize();
+    ShuntingYard shunt(tokens);
+    tokens = shunt.Parse();
+    TokenHandler handler(tokens);
+    handler.Print();
     return 0;
 }
