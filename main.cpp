8e4202a09dea90317a6d36edcbe2898dc0fcf746
ruwix <grantpauker@gmail.com>
2018-04-21 17:16:01 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,30 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <array>
+
 #include "include/token.hpp"
 #include "include/number_token.hpp"
-
 #include "include/tokenizer.hpp"
 #include "include/symbols.hpp"
 #include "include/token_handler.hpp"
 #include "include/shunting_yard.hpp"
-#include "include/expression.hpp"
+#include "include/tree.hpp"
+#include "include/node.hpp"
+
+#define num(n) new NumberToken(n)
+#define node(n) new Node<Token *>(num(n))
 
 int main()
 {
-    // std::string expr("12+34/56%7&89");
-    std::vector<Token *> tmp;
-    for (int i = 0; i < 10; i++)
-    {
-        tmp.push_back(new NumberToken(i));
-    }
+    Expression tr;
+    tr.head = node(0);
+    tr.head->left = node(1);
+    tr.head->right = node(2);
+    tr.Insert(num(3));
+    // tr.PrintTree();
 
-    Expression *exp = new Expression(tmp[0]);
-    exp->PrintOwn();
-    exp->InsertRight(tmp[1]);
-    exp->GetRight()->PrintOwn();
-
-    std::string expr("1+2*3");
+    std::string expr("12+34/56%7&89"); //34/36%7+12&89
     Tokenizer t(expr);
     std::vector<Token *> tokens = t.Tokenize();
     ShuntingYard shunt(tokens);
